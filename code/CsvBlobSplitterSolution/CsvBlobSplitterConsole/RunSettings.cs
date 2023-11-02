f760444e6434f495ba62ed36fecb15e420a4453f
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-02 10:31:32 -0400

    Parse env variables

--- code/CsvBlobSplitterSolution/CsvBlobSplitterConsole/RunSettings.cs
+++ code/CsvBlobSplitterSolution/CsvBlobSplitterConsole/RunSettings.cs
@@ -8,12 +8,157 @@ namespace CsvBlobSplitterConsole
 {
     internal class RunSettings
     {
-        public string Source { get; set; } = string.Empty;
+        public Uri SourceBlob { get; }
 
-        public string Destination { get; set; } = string.Empty;
+        public Uri? DestinationBlobPrefix { get; }
 
-        public BlobCompression Compression { get; set; } = BlobCompression.None;
+        public BlobCompression Compression { get; }
 
-        public bool HasHeaders { get; set; } = true;
+        public bool HasCsvHeaders { get; }
+
+        #region Constructors
+        public static RunSettings FromEnvironmentVariables()
+        {
+            var sourceBlob = GetUri("SourceBlob");
+            var destinationBlobPrefix = GetUri("DestinationBlobPrefix", false);
+            var compression = GetEnum<BlobCompression>("Compression", false);
+            var hasCsvHeaders = GetBool("HasCsvHeaders", false);
+
+            return new RunSettings(
+                sourceBlob,
+                destinationBlobPrefix,
+                compression,
+                hasCsvHeaders);
+        }
+
+        public RunSettings(
+            Uri sourceBlob,
+            Uri? destinationBlobPrefix,
+            BlobCompression? compression,
+            bool? hasCsvHeaders)
+        {
+            SourceBlob = sourceBlob;
+            DestinationBlobPrefix = destinationBlobPrefix;
+            Compression = compression ?? BlobCompression.None;
+            HasCsvHeaders = hasCsvHeaders ?? true;
+        }
+        #endregion
+
+        #region Environment variables
+        #region String
+        private static string GetString(string variableName)
+        {
+            var value = GetString(variableName, true);
+
+            return value!;
+        }
+
+        private static string? GetString(string variableName, bool mustExist)
+        {
+            var value = Environment.GetEnvironmentVariable(variableName);
+
+            if (mustExist && value == null)
+            {
+                throw new ArgumentNullException(variableName, "Environment variable missing");
+            }
+
+            return value;
+        }
+        #endregion
+
+        #region Uri
+        private static Uri GetUri(string variableName)
+        {
+            var uri = GetUri(variableName, true);
+
+            return uri!;
+        }
+
+        private static Uri? GetUri(string variableName, bool mustExist)
+        {
+            var value = Environment.GetEnvironmentVariable(variableName);
+
+            if (mustExist && value == null)
+            {
+                throw new ArgumentNullException(variableName, "Environment variable missing");
+            }
+
+            if (value != null)
+            {
+                try
+                {
+                    var uri = new Uri(value, UriKind.Absolute);
+
+                    return uri;
+                }
+                catch
+                {
+                    throw new ArgumentNullException(variableName, $"Unsupported value:  '{value}'");
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+
+        #region Enum
+        private static T? GetEnum<T>(string variableName, bool mustExist)
+            where T : struct
+        {
+            var value = Environment.GetEnvironmentVariable(variableName);
+
+            if (mustExist && value == null)
+            {
+                throw new ArgumentNullException(variableName, "Environment variable missing");
+            }
+
+            if (value != null)
+            {
+                if (Enum.TryParse<T>(value, out var enumValue))
+                {
+                    return enumValue;
+                }
+                else
+                {
+                    throw new ArgumentNullException(variableName, $"Unsupported value:  '{value}'");
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+
+        #region Bool
+        private static bool? GetBool(string variableName, bool mustExist)
+        {
+            var value = Environment.GetEnvironmentVariable(variableName);
+
+            if (mustExist && value == null)
+            {
+                throw new ArgumentNullException(variableName, "Environment variable missing");
+            }
+
+            if (value != null)
+            {
+                if (bool.TryParse(value, out var boolValue))
+                {
+                    return boolValue;
+                }
+                else
+                {
+                    throw new ArgumentNullException(variableName, $"Unsupported value:  '{value}'");
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+        #endregion
     }
 }

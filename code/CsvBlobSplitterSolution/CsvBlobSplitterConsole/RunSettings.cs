2478ecbe04a168ec0c9b28aa4a58f58e665b570d
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-02 09:24:37 -0400

    Remove command line support

---
+++ code/CsvBlobSplitterSolution/CsvBlobSplitterConsole/RunSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvBlobSplitterConsole
+{
+    internal class RunSettings
+    {
+        public string Source { get; set; } = string.Empty;
+
+        public string Destination { get; set; } = string.Empty;
+
+        public BlobCompression Compression { get; set; } = BlobCompression.None;
+
+        public bool HasHeaders { get; set; } = true;
+    }
+}

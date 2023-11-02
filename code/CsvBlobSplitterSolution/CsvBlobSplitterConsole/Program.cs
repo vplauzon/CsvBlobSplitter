f9e3c45c04ad909285cc2e635dc8cad8175070d7
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-02 18:11:07 -0400

    .

--- code/CsvBlobSplitterSolution/CsvBlobSplitterConsole/Program.cs
+++ code/CsvBlobSplitterSolution/CsvBlobSplitterConsole/Program.cs
@@ -5,6 +5,17 @@
         private static async Task Main(string[] args)
         {
             var runSettings = RunSettings.FromEnvironmentVariables();
+
+            Console.WriteLine();
+            Console.WriteLine($"SourceBlob:  {runSettings.SourceBlob}");
+            Console.WriteLine($"DestinationBlobPrefix:  {runSettings.DestinationBlobPrefix}");
+            Console.WriteLine($"Compression:  {runSettings.Compression}");
+            Console.WriteLine($"HasCsvHeaders:  {runSettings.HasCsvHeaders}");
+            Console.WriteLine($"MaxRowsPerShard:  {runSettings.MaxRowsPerShard}");
+            Console.WriteLine($"MaxMbPerShard:  {runSettings.MaxMbPerShard}");
+            Console.WriteLine($"MaxMbPerShard:  {runSettings.MaxMbPerShard}");
+            Console.WriteLine();
+
             var etl = EtlFactory.Create(runSettings);
 
             await etl.ProcessAsync();

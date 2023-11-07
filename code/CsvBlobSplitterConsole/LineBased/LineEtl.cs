233994b16cd75760a8d5e366e91ae7d5dddc844b
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-07 12:01:57 -0500

    Add Line ETL

---
+++ code/CsvBlobSplitterConsole/LineBased/LineEtl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvBlobSplitterConsole.LineBased
+{
+    internal class LineEtl : IEtl
+    {
+        Task IEtl.ProcessAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

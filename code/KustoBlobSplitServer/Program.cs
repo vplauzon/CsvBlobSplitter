9b97f3afcf8ec384a3447ac17a620bc70e6dd3cb
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-11 13:52:49 -0500

    Integrate with server

--- code/KustoBlobSplitServer/Program.cs
+++ code/KustoBlobSplitServer/Program.cs
@@ -1,8 +1,11 @@
+using KustoBlobSplitLib;
+using Microsoft.Extensions.Hosting;
+
 namespace KustoBlobSplitServer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +18,12 @@ namespace KustoBlobSplitServer
                 return string.Empty;
             });
 
-            app.Run();
+            var webServerTask = app.RunAsync();
+
+            await EtlRun.RunEtlAsync();
+
+            //  Stop web server
+            await Task.WhenAll(app.StopAsync(), webServerTask);
         }
     }
 }

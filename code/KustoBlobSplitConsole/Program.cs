5bf49fd00b353178c4aeb33f22c37de48ca51391
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-11 13:44:59 -0500

    Move run to EtlRun

--- code/KustoBlobSplitConsole/Program.cs
+++ code/KustoBlobSplitConsole/Program.cs
@@ -7,28 +7,7 @@ namespace KustoBlobSplitConsole
     {
         private static async Task Main(string[] args)
         {
-            var stopwatch = new Stopwatch();
-            var runSettings = RunSettings.FromEnvironmentVariables();
-
-            stopwatch.Start();
-            Console.WriteLine();
-            Console.WriteLine($"AuthMode:  {runSettings.AuthMode}");
-            Console.WriteLine($"ManagedIdentityResourceId:  {runSettings.ManagedIdentityResourceId}");
-            Console.WriteLine($"Format:  {runSettings.Format}");
-            Console.WriteLine($"SourceBlob:  {runSettings.SourceBlob}");
-            Console.WriteLine($"DestinationBlobPrefix:  {runSettings.DestinationBlobPrefix}");
-            Console.WriteLine($"Compression:  {runSettings.InputCompression}");
-            Console.WriteLine($"Compression:  {runSettings.OutputCompression}");
-            Console.WriteLine($"HasHeaders:  {runSettings.HasHeaders}");
-            Console.WriteLine($"MaxMbPerShard:  {runSettings.MaxMbPerShard}");
-            Console.WriteLine();
-            Console.WriteLine($"Core count:  {Environment.ProcessorCount}");
-            Console.WriteLine();
-
-            var etl = EtlFactory.Create(runSettings);
-
-            await etl.ProcessAsync();
-            Console.WriteLine($"ETL completed in {stopwatch.Elapsed}");
+            await EtlRun.RunEtlAsync();
         }
     }
 }
---
+++ code/KustoBlobSplitLib/EtlRun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoBlobSplitLib
+{
+    public static class EtlRun
+    {
+        public static async Task RunEtlAsync()
+        {
+            var stopwatch = new Stopwatch();
+            var runSettings = RunSettings.FromEnvironmentVariables();
+
+            stopwatch.Start();
+            Console.WriteLine();
+            Console.WriteLine($"AuthMode:  {runSettings.AuthMode}");
+            Console.WriteLine($"ManagedIdentityResourceId:  {runSettings.ManagedIdentityResourceId}");
+            Console.WriteLine($"Format:  {runSettings.Format}");
+            Console.WriteLine($"SourceBlob:  {runSettings.SourceBlob}");
+            Console.WriteLine($"DestinationBlobPrefix:  {runSettings.DestinationBlobPrefix}");
+            Console.WriteLine($"Compression:  {runSettings.InputCompression}");
+            Console.WriteLine($"Compression:  {runSettings.OutputCompression}");
+            Console.WriteLine($"HasHeaders:  {runSettings.HasHeaders}");
+            Console.WriteLine($"MaxMbPerShard:  {runSettings.MaxMbPerShard}");
+            Console.WriteLine();
+            Console.WriteLine($"Core count:  {Environment.ProcessorCount}");
+            Console.WriteLine();
+
+            var etl = EtlFactory.Create(runSettings);
+
+            await etl.ProcessAsync();
+            Console.WriteLine($"ETL completed in {stopwatch.Elapsed}");
+        }
+    }
+}

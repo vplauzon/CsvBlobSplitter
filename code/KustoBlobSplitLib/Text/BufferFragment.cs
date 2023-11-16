9e61e2fd9b07415c3ccbc037a330524a5e76f62a
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-16 08:20:35 -0500

    Add BufferFragment

---
+++ code/KustoBlobSplitLib/Text/BufferFragment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoBlobSplitLib.Text
+{
+    internal class BufferFragment : IEnumerable<byte>
+    {
+        private readonly byte[] _data;
+        private readonly int _offset;
+
+        public BufferFragment(int length)
+        {
+            _data = new byte[length];
+            _offset = 0;
+            Length = length;
+        }
+
+        public int Length { get; }
+
+        #region IEnumerable<byte>
+        IEnumerator<byte> IEnumerable<byte>.GetEnumerator()
+        {
+            var end = _offset + Length;
+
+            for (int i = _offset; i != end; ++i)
+            {
+                yield return _data[i % _data.Length];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<byte>)this).GetEnumerator();
+        }
+        #endregion
+    }
+}

8f12af5ac799333345e4fa7d07639e04ecc7282c
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-16 09:36:34 -0500

    .

--- code/KustoBlobSplitLib/Text/BufferFragment.cs
+++ code/KustoBlobSplitLib/Text/BufferFragment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KustoBlobSplitLib.LineBased;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,16 @@ namespace KustoBlobSplitLib.Text
 {
     internal class BufferFragment : IEnumerable<byte>
     {
-        private readonly byte[] _data;
+        private readonly byte[] _buffer;
         private readonly int _offset;
 
         #region Constructors
         private BufferFragment(
-            byte[] data,
+            byte[] buffer,
             int offset,
             int length)
         {
-            _data = data;
+            _buffer = buffer;
             _offset = offset;
             Length = length;
         }
@@ -46,6 +47,7 @@ namespace KustoBlobSplitLib.Text
 
         public bool Any() => Length > 0;
 
+        #region Merge
         public BufferFragment Merge(BufferFragment other)
         {
             if (Length == 0)
@@ -58,7 +60,7 @@ namespace KustoBlobSplitLib.Text
             }
             else
             {
-                if (!object.ReferenceEquals(_data, other._data))
+                if (!object.ReferenceEquals(_buffer, other._buffer))
                 {
                     throw new ArgumentException(nameof(other), "Not related to same buffer");
                 }
@@ -66,14 +68,49 @@ namespace KustoBlobSplitLib.Text
                 var left = _offset < other._offset ? this : other;
                 var right = _offset > other._offset ? other : this;
 
-                if (left._offset + left.Length % _data.Length != right._offset)
+                if (left._offset + left.Length % _buffer.Length != right._offset)
                 {
                     throw new ArgumentException(nameof(other), "Not contiguous");
                 }
 
-                return new BufferFragment(_data, left._offset, left.Length + right.Length);
+                return new BufferFragment(_buffer, left._offset, left.Length + right.Length);
             }
         }
+        #endregion
+
+        #region Splice
+        /// <summary>This includes the specified index and everything before.</summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public BufferFragment SpliceBefore(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return new BufferFragment(_buffer, _offset, index + 1);
+        }
+
+        /// <summary>This excludes the specified index and includes everything after.</summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public BufferFragment SpliceAfter(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            if (index == Length - 1)
+            {
+                return Empty;
+            }
+            else
+            {
+                return new BufferFragment(_buffer, _offset + index + 1, Length - index - 1);
+            }
+        }
+        #endregion
 
         #region IEnumerable<byte>
         IEnumerator<byte> IEnumerable<byte>.GetEnumerator()
@@ -82,7 +119,7 @@ namespace KustoBlobSplitLib.Text
 
             for (int i = _offset; i != end; ++i)
             {
-                yield return _data[i % _data.Length];
+                yield return _buffer[i % _buffer.Length];
             }
         }
 

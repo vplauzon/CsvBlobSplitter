353cc56bac6f19216d9ff774883b37409037d688
vplauzon <vincent_lauzon@yahoo.com.au>
2023-11-16 09:20:14 -0500

    .

--- code/KustoBlobSplitLib/Text/BufferFragment.cs
+++ code/KustoBlobSplitLib/Text/BufferFragment.cs
@@ -12,16 +12,68 @@ namespace KustoBlobSplitLib.Text
         private readonly byte[] _data;
         private readonly int _offset;
 
-        public BufferFragment(int length)
+        #region Constructors
+        private BufferFragment(
+            byte[] data,
+            int offset,
+            int length)
         {
-            _data = new byte[length];
-            _offset = 0;
+            _data = data;
+            _offset = offset;
             Length = length;
         }
+
+        public static BufferFragment Create(int length)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), length.ToString());
+            }
+            else if (length == 0)
+            {
+                return Empty;
+            }
+            else
+            {
+                return new BufferFragment(new byte[length], 0, length);
+            }
+        }
+        #endregion
+
+        public static BufferFragment Empty { get; } = new BufferFragment(new byte[0], 0, 0);
 
         public int Length { get; }
 
         public bool Any() => Length > 0;
+
+        public BufferFragment Merge(BufferFragment other)
+        {
+            if (Length == 0)
+            {
+                return other;
+            }
+            else if (other.Length == 0)
+            {
+                return this;
+            }
+            else
+            {
+                if (!object.ReferenceEquals(_data, other._data))
+                {
+                    throw new ArgumentException(nameof(other), "Not related to same buffer");
+                }
+
+                var left = _offset < other._offset ? this : other;
+                var right = _offset > other._offset ? other : this;
+
+                if (left._offset + left.Length % _data.Length != right._offset)
+                {
+                    throw new ArgumentException(nameof(other), "Not contiguous");
+                }
+
+                return new BufferFragment(_data, left._offset, left.Length + right.Length);
+            }
+        }
 
         #region IEnumerable<byte>
         IEnumerator<byte> IEnumerable<byte>.GetEnumerator()
